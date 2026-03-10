70e978148ce21984f26958fa9ccfdb19ddd8b388
siva4github <leo.shiv@gmail.com>
2026-03-10 10:36:24 -0400

    added repositories

---
+++ src/BlazorDeepDive/ServerManagement/Models/CitiesRepositories.cs
@@ -0,0 +1,16 @@
+﻿namespace ServerManagement.Models
+{
+    public static class CitiesRepositories
+    {
+        private static List<string> cities = new List<string>()
+        {
+            "Toronto",
+            "Montreal",
+            "Ottawa",
+            "Calgary",
+            "Halifax"
+        };
+
+        public static List<string> GetCities() => cities;
+    }
+}
